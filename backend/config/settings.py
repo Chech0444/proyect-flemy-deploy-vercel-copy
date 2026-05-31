8affb8d2689380a005862c3def04706858fd8801
Checho <666destroy1001@gmail.com>
2026-05-31 13:47:39 -0500

    arreglo en cors

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -211,6 +211,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",
     "http://127.0.0.1:4200",
+    "https://proyect-flemy-deploy-vercel-copy.vercel.app",
 ]
 
 # ====================== RESTO DE TU CONFIGURACIÓN (sin duplicados) ======================
